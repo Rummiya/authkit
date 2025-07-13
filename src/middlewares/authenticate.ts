acff54445e9ef6ca1cceb9f78a065267d7e22e62
Rummiya <rumibashirova@gmail.com>
2025-07-13 23:21:41 +0600

    fix(middleware): add return to avoid response error

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,39 +1,45 @@
-import type { User } from "@prisma/client";
-import type { NextFunction, Request, Response } from "express";
+import type { User } from '@prisma/client';
+import type { NextFunction, Request, Response } from 'express';
 
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
-import { prisma } from "../lib/prisma-client";
+import { prisma } from '../lib/prisma-client';
 
-export async function authenticate(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(" ")[1];
+export async function authenticate(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	const authHeader = req.headers.authorization;
+	const token = authHeader && authHeader.split(' ')[1];
 
-  if (!token) {
-    return res.status(401).json({ error: "Вы не зарегистрированы" });
-  }
+	if (!token) {
+		res.status(401).json({ error: 'Вы не зарегистрированы' });
+		return;
+	}
 
-  try {
-    const decoded = jwt.verify(token, process.env.SECRET_KEY || "secret_key") as User;
+	try {
+		const decoded = jwt.verify(
+			token,
+			process.env.SECRET_KEY || 'secret_key'
+		) as User;
 
-    // Проверка, существует ли пользователь в базе
-    const user = await prisma.user.findUnique({
-      where: { id: decoded.id },
-    });
+		// Проверка, существует ли пользователь в базе
+		const user = await prisma.user.findUnique({
+			where: { id: decoded.id },
+		});
 
-    if (!user) {
-      return res
-        .status(401)
-        .json({ error: "Токен действителен, но пользователь не найден" });
-    }
+		if (!user) {
+			res
+				.status(401)
+				.json({ error: 'Токен действителен, но пользователь не найден' });
+			return;
+		}
 
-    req.user = decoded;
-    next();
-  }
-  catch (err) {
-    console.error("JWT error:", err);
-    return res
-      .status(403)
-      .json({ error: "Недействительный или просроченный токен" });
-  }
+		req.user = decoded;
+		next();
+	} catch (err) {
+		console.error('JWT error:', err);
+		res.status(403).json({ error: 'Недействительный или просроченный токен' });
+	}
 }
