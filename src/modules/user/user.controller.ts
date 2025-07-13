10ddd6b20c6a84cb360c2193bc5c71d6e3704b5c
Rummiya <rumibashirova@gmail.com>
2025-07-13 23:42:08 +0600

    fix(user): return 404 if user not found by ID

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,35 +1,38 @@
-import type { NextFunction, Request, Response } from "express";
+import type { NextFunction, Request, Response } from 'express';
 
-import { prisma } from "../../lib/prisma-client";
+import { prisma } from '../../lib/prisma-client';
 
 export const userController = {
-  getAllUsers: async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const users = await prisma.user.findMany({});
-      res.json({ data: users });
-    }
-    catch (error) {
-      next(error);
-    }
-  },
-  getUserById: async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const id = req.params.id;
+	getAllUsers: async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const users = await prisma.user.findMany({});
+			res.json({ data: users });
+		} catch (error) {
+			next(error);
+		}
+	},
+	getUserById: async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const id = req.params.id;
 
-      if (!id) {
-        res.status(400).json({ error: "ID пользователя не передан" });
-      }
+			if (!id) {
+				res.status(400).json({ error: 'ID пользователя не передан' });
+			}
 
-      const user = await prisma.user.findUnique({
-        where: {
-          id,
-        },
-      });
+			const user = await prisma.user.findUnique({
+				where: {
+					id,
+				},
+			});
 
-      res.json({ data: user });
-    }
-    catch (error) {
-      next(error);
-    }
-  },
+			if (!user) {
+				res.status(404).json({ error: 'Пользователь не найден!' });
+				return;
+			}
+
+			res.json({ data: user });
+		} catch (error) {
+			next(error);
+		}
+	},
 };
