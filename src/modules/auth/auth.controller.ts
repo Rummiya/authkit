9fe7edc6411182672eba95abfcf97c5d9e554fac
Rummiya <rumibashirova@gmail.com>
2025-07-13 23:32:21 +0600

    feat(auth): implement login controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import type { NextFunction, Request, Response } from 'express';
 
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 import { prisma } from '../../lib/prisma-client';
 
@@ -28,6 +29,49 @@ export const authController = {
 			});
 
 			res.json({ data: user, message: 'Вы успешно зарегистрированы!' });
+		} catch (error) {
+			next(error);
+		}
+	},
+	login: async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const { email, password } = req.body;
+
+			if (!email || !password) {
+				res.status(400).json({ error: 'Заполните все поля' });
+				return;
+			}
+
+			const user = await prisma.user.findUnique({
+				where: {
+					email,
+				},
+			});
+
+			if (!user) {
+				res.status(400).json({ error: 'Неверный логин или пароль' });
+				return;
+			}
+
+			const isPasswordCompare = await bcrypt.compare(password, user.password);
+
+			if (!isPasswordCompare) {
+				res.status(400).json({ error: 'Неверный логин или пароль' });
+				return;
+			}
+
+			const token = jwt.sign(
+				{ userId: user.id },
+				process.env.SECRET_KEY || '123'
+			);
+
+			res.json({
+				data: {
+					token,
+					user,
+				},
+				message: 'Вы успешно вошли!',
+			});
 		} catch (error) {
 			next(error);
 		}
