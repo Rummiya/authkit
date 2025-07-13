13e720b6996bba4ba057ec5711a6c92b44e0516c
Rummiya <rumibashirova@gmail.com>
2025-07-13 18:38:38 +0600

    feat(auth): add authenticate middleware

---
+++ src/middlewares/authenticate.ts
@@ -0,0 +1,39 @@
+import type { User } from "@prisma/client";
+import type { NextFunction, Request, Response } from "express";
+
+import jwt from "jsonwebtoken";
+
+import { prisma } from "../lib/prisma-client";
+
+export async function authenticate(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Вы не зарегистрированы" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY || "secret_key") as User;
+
+    // Проверка, существует ли пользователь в базе
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.id },
+    });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ error: "Токен действителен, но пользователь не найден" });
+    }
+
+    req.user = decoded;
+    next();
+  }
+  catch (err) {
+    console.error("JWT error:", err);
+    return res
+      .status(403)
+      .json({ error: "Недействительный или просроченный токен" });
+  }
+}
