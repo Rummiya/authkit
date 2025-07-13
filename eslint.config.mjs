788ebaf59a472010cf63cd10b8c787d2acc89f91
Rummiya <rumibashirova@gmail.com>
2025-07-13 13:21:24 +0600

    chore(eslint): disable unnecessary rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,37 @@
-import antfu from "@antfu/eslint-config";
+import antfu from '@antfu/eslint-config';
 
-export default antfu({
-  type: "app",
-  typescript: true,
-  formatters: true,
-  stylistic: {
-    indent: 2,
-    semi: true,
-    quotes: "double",
-  },
-}, {
-  rules: {
-    "ts/no-redeclare": "off",
-    "no-console": ["warn"],
-    "antfu/no-top-level-await": ["off"],
-    "node/prefer-global/process": ["off"],
-    "perfectionist/sort-imports": ["error", {
-      tsconfigRootDir: ".",
-    }],
-    "unicorn/filename-case": ["error", {
-      case: "kebabCase",
-      ignore: ["README.md"],
-    }],
-    "test/prefer-lowercase-title": ["off"],
-  },
-});
+export default antfu(
+	{
+		type: 'app',
+		typescript: true,
+		formatters: true,
+		stylistic: {
+			indent: 2,
+			semi: true,
+			quotes: 'double',
+		},
+	},
+	{
+		rules: {
+			'ts/no-redeclare': 'off',
+			'no-console': ['warn'],
+			'antfu/no-top-level-await': ['off'],
+			'node/prefer-global/process': ['off'],
+			'perfectionist/sort-imports': [
+				'error',
+				{
+					tsconfigRootDir: '.',
+				},
+			],
+			'unicorn/filename-case': [
+				'error',
+				{
+					case: 'kebabCase',
+					ignore: ['README.md'],
+				},
+			],
+			'test/prefer-lowercase-title': ['off'],
+			'style/no-tabs': 'off',
+		},
+	}
+);
