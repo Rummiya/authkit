6394d04a412b795c1e2be5065ea6ddac9b026c81
Rummiya <rumibashirova@gmail.com>
2025-07-13 19:04:16 +0600

    feat(auth): implement registration controller

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,34 @@
+import type { NextFunction, Request, Response } from 'express';
+
+import bcrypt from 'bcryptjs';
+
+import { prisma } from '../../lib/prisma-client';
+
+export const authController = {
+	registration: async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const { fullname, email, password, birthday } = req.body;
+
+			const existingUser = await prisma.user.findUnique({ where: { email } });
+
+			if (existingUser) {
+				return res.status(400).json({ error: 'Пользователь уже существует' });
+			}
+
+			const hashedPassword = await bcrypt.hash(password, 10);
+
+			const user = await prisma.user.create({
+				data: {
+					email,
+					password: hashedPassword,
+					birthday,
+					fullname,
+				},
+			});
+
+			res.json({ data: user, message: 'Вы успешно зарегистрированы!' });
+		} catch (error) {
+			next(error);
+		}
+	},
+};
